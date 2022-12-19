1ecef5dc10e7d11dffeff7be8afe8ec2a401fd0b
Sandromaia87 <sandro_maia_mattos@hotmail.com>
2022-12-19 12:24:52 -0300

    Merge branch 'QuintaetapaCRUD' of https://github.com/sandromaia87/laravel_laura into QuintaetapaCRUD

--- database/migrations/2022_12_13_130650_create_championships_table.php
+++ database/migrations/2022_12_13_130650_create_championships_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('championship', function (Blueprint $table) {
+        Schema::create('championships', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
             $table->string('name')->unique();
