6e601267740f78ee77b2b1b3cc5165ccc552f321
Sandromaia87 <sandro_maia_mattos@hotmail.com>
2022-12-21 15:32:44 -0300

    Criação migrate, model, Controllers Mudanca do exibicao do show.blade.php

---
+++ resources/views/date_champioship/create.blade.php
@@ -0,0 +1,49 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Meus Campeonatos') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+          <form class="card" action="{{ route('championship.store') }}" method="POST">
+            @csrf
+            <div class="card-header">
+              <h3 class="card-title">Novo Campeonato</h3>
+            </div>
+            <div class="card-body">
+              <div class="mb-3">
+                <label class="form-label required">Nome</label>
+                <div>
+                  <input type="text" class="form-control" id="name" name="name">
+                  <small class="form-hint">Este nome estará visível para os participantes</small>
+                </div>
+              </div>
+              <div class="mb-3">
+                <label class="form-label required">Email</label>
+                <div>
+                  <input type="text" class="form-control" id="email" name="email">
+                  <small class="form-hint">Este email estará visível para os participantes</small>
+                </div>
+              </div>
+              <div class="mb-3">
+                <label class="form-label">Tipo</label>
+                <div>
+                  <select class="form-select" id="type" name="type"> 
+                    <option value="1">Crossfit</option>
+                    <option value="2">Corrida</option>
+                    <option value="3">Futevolei</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div class="card-footer text-end">
+              <button type="submit" class="btn btn-primary">Criar</button>
+            </div>
+          </form>
+
+        </div>
+      </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/date_champioship/edit.blade.php
@@ -0,0 +1,40 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Meus Campeonatos') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+          <form class="card" action="{{ route('championship.update',$championship) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="card-header">
+                <h3 class="card-title">{{ $championship->name }}
+            </div>
+            <div class="card-body">
+              <div class="mb-3">
+                <label class="form-label required">Nome</label>
+                <div>
+                  <input type="text" class="form-control" id="name" name="name" value="{{ $championship->name }}">
+                  <small class="form-hint">Este nome estará visível para os participantes</small>
+                </div>
+              </div>
+              <div class="mb-3">
+                <label class="form-label">Email</label>
+                <div>
+                  <input type="text" class="form-control" id="email" name="email" value="{{ $championship->email }}">
+                  <small class="form-hint">Será usado como contato extra no seu campeonato</small>
+                </div>
+              </div>
+            </div>
+            <div class="card-footer text-end">
+              <button type="submit" class="btn btn-primary">Editar</button>
+            </div>
+          </form>
+
+        </div>
+      </div>
+    </div>
+</x-app-layout>
