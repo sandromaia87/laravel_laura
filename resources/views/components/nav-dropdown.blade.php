be5eccf56845c80f3f15feb038c8c2e0dc46454a
Sandromaia87 <sandro_maia_mattos@hotmail.com>
2023-01-16 15:54:08 -0300

    ajustes

---
+++ resources/views/components/nav-dropdown.blade.php
@@ -0,0 +1,22 @@
+@props(['contentClasses' => 'block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out'])
+
+<div class="relative" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
+    <div @click="open = ! open">
+        {{ $trigger }}
+    </div>
+
+    <div x-show="open"
+            x-transition:enter="transition ease-out duration-200"
+            x-transition:enter-start="transform opacity-0 scale-95"
+            x-transition:enter-end="transform opacity-100 scale-100"
+            x-transition:leave="transition ease-in duration-75"
+            x-transition:leave-start="transform opacity-100 scale-100"
+            x-transition:leave-end="transform opacity-0 scale-95"
+            class="rounded-md shadow-lg"
+            style="display: none;"
+            @click="open = false">
+        <div class="{{ $contentClasses }}">
+            {{ $content }}
+        </div>
+    </div>
+</div>
